7e1b90858e044bfd51ce0882a4525ca63ed90295
Calvin Wang <calvinkw1@gmail.com>
2015-03-16 18:12:57 -0700

    js bubble sort done

--- sort.js
+++ sort.js
@@ -1,7 +1,22 @@
 function bubbleSort(arr){
-
+  var i,
+      count;
+  for (count = 0; count < arr.length-1; count++) {
+      console.log(count);
+      i=0;
+    for (i = 0; i < arr.length-1; i++) {
+      if (arr[i] > arr[i+1]) {
+        var temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+        console.log(arr);
+      }
+    }
+  }
+  return arr;
 }
 
+
 // do not touch this
-module.exports = bubbleSort
+module.exports = bubbleSort;
 
